578f4d5a7cdbbb563bcd3f10c7701c787db7da14
tekgirl77 <tekgirl77@gmail.com>
2015-12-05 12:41:39 -0700

    LESS is working, installed Gulp

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var lessMiddleware = require('less-middleware');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var debug = require('debug')('NodeJS_Fun:server');
 
 var app = express();
+
+//Create Constructor here.
+// 'use strict';
 
 // set port via env variable or static
 app.set('port', process.env.PORT || 3000);
@@ -23,24 +27,33 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, '/public')));
+//app.use(require('less-middleware')(path.join(__dirname, '/public')));
+debug("LESS path is: " + path.join(__dirname, '/less'));
+debug("CSS path is: " + path.join(__dirname, 'public'));
+app.use(lessMiddleware(path.join(__dirname, '/less'), {
+    dest: path.join(__dirname, 'public'),
+    force: true
+    //debug: true
+}));
+//app.use(express.static(__dirname + "/public"));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.all('*', function(req,res) {
-  res.render(
-      'index',
-      {title: ' Salle\'s \{Slice\}!'}
-  );
-})
+    res.render(
+        'index', {
+            title: 'Welcome to my sandbox!'//' Salle\'s \{Slice\}!'
+        }
+    );
+});
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -48,23 +61,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
