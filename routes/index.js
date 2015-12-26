c579614a41f299fddc66f00e837edbf6bd4aab42
tekgirl77 <tekgirl77@gmail.com>
2015-12-26 13:52:54 -0700

    Adding header check functionality

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ router.get('/about', function (req, res, next) {
     res.render('about', {title: 'A Little About Me'
     });
 });
+router.get('/headers', function (req, res, next) {
+    res.set('Content-Type', 'text/plain');
+    var s = '',
+        name;
+    if (req.headers) {
+        for (name in req.headers) {
+            s += name + ": " + req.headers[name] + "\n";
+        }
+        res.send(s);
+    }
+});
 
 
 module.exports = router;
